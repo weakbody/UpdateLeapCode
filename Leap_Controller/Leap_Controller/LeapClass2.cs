3cdfe43593b7dec551c5a28dcbe1896f54c35988
weakbody <cmp8448@uncw.edu>
2013-10-21 16:53:32 -0400

    update

---
+++ Leap_Controller/Leap_Controller/LeapClass2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Leap_C
+{
+    class LeapClass2
+    {
+        public void Init_AR_Drone()
+        {
+
+        }
+
+        public int Roll_AR_Drone()
+        {
+            return 1;
+        }
+
+        public void setPitch(float pitch)
+        {
+
+        }
+
+        public void setYaw(float yaw)
+        {
+
+        }
+
+        public void set_X_Y(float x, float y)
+        {
+
+        }
+
+        public void set_Roll(float roll)
+        {
+
+        }
+
+        public void AR_Drone_Takeoff()
+        {
+
+        }
+
+        public void AR_Drone_Land()
+        {
+
+        }
+
+        public void AR_Drone_FlatTrim()
+        {
+
+        }
+    }
+}
